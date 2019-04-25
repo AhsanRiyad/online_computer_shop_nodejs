421560736a1fee2df0179b759b31a9ada8bb1ea3
Ahsan Riyad <riyad298@gmail.com>
2019-04-25 13:05:06 +0600

    commit by alias
    this is used for short cut puspose

--- model/userModel.js
+++ model/userModel.js
@@ -32,6 +32,11 @@ module.exports={
 
 		var  sql = "INSERT INTO user_table(u_password, u_email, u_mobile, dob, u_status, u_type, last_name) VALUES (:password,:email,:phone,to_char(sysdate , 'MM-DD-YYYY'),'valid',:type,:last_name)";
 
+
+
+		INSERT INTO user_table(u_id ,  u_password, u_email, u_mobile, dob, u_status, u_type, last_name) VALUES (:password,:email,:phone,to_char(sysdate , 'MM-DD-YYYY'),'valid',:type,:last_name)
+
+
 		var params = {
 			password : { val: user.password },
 			email : { val: user.email },

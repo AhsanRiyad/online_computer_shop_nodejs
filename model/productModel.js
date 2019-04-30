e5bcbd4e84b13f04d90dc2daa191cf8fabe61380
AhsanRiyad <riyad298@gmail.com>
2019-04-30 06:41:17 +0600

    cart total amount view added

--- model/productModel.js
+++ model/productModel.js
@@ -225,7 +225,7 @@ module.exports={
 
 		var sql = "SELECT COUNT(*) as cart_count  FROM cart WHERE user_id =:uuid";
 
-
+		//var sql = "select cart_count from cart_count where USER_ID = :uuid;"
 		console.log('cart_count'+' ' + uid);
 		console.log(sql);
 		var params = {
@@ -255,7 +255,10 @@ module.exports={
 		//var sql = "call cartPage("+uid+")";
 		
 
-		var sql = "select SUM(pr.product_price*p.product_qntity) as total from cart c , p_include_cart p , products pr where p.cart_id = c.cart_id and p.product_id = pr.product_id and c.user_id = :uuid";
+		var sql1 = "select SUM(pr.product_price*p.product_qntity) as total from cart c , p_include_cart p , products pr where p.cart_id = c.cart_id and p.product_id = pr.product_id and c.user_id = :uuid";
+
+		var sql = "select * from cart_total_amount where uuid = :uuid";
+
 		console.log(sql);
 		var params = [ uid ];
 
--- sql files/views.sql
+++ sql files/views.sql
@@ -26,8 +26,34 @@ select c.user_id as uuid ,  p.product_id , pr.product_name , pr.product_price , 
 
 /
 
+drop view cart_count;
+
+/
+create or replace view cart_count as
+select user_id ,  count(*) as cart_count from cart group by user_id;
 
 
+/
+
+
+select cart_count from cart_count where USER_ID = 1;
+
+/
+
+/
+
+drop view cart_total_amount;
+/
+create or replace view cart_total_amount as
+select c.user_id as uuid , SUM(pr.product_price*p.product_qntity) as total from cart c , p_include_cart p , products pr where p.cart_id = c.cart_id and p.product_id = pr.product_id group by c.user_id;
+
+
+/
+
+select * from cart_total_amount where uuid = 1;
+
+
+/
 
 commit;
 
